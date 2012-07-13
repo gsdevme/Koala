e8bc5f277c811b9928c02fd566ea9f378a8b8e45
Gavin Staniforth <gavshouse90@gmail.com>
2012-07-13 23:00:14 +0100

    Registry test with quick shell script

---
+++ tests/Koala/RegistryTest.php
@@ -0,0 +1,59 @@
+<?php
+
+	namespace tests\Koala;
+
+	require_once realpath(__DIR__) . '/../../Koala/Interfaces/Registry.php';
+	require_once realpath(__DIR__) . '/../../Koala/Registry.php';
+
+	class RegistryTest extends \PHPUnit_Framework_TestCase
+	{
+
+		private $_registry;
+
+		public function setUp()
+		{
+			$this->_registry = \Koala\Registry::getInstance();
+		}
+
+		/**
+		 * @dataProvider setProvider
+		 */
+		public function testSet($key, $value)
+		{
+			$this->_registry->set($key, $value);
+			$this->assertTrue(isset($this->_registry->$key));
+
+			$setValue = $this->_registry->get($key);
+
+			$this->assertTrue(($value === $setValue));
+		}
+
+		/**
+		 * @expectedException LogicException
+		 */
+		public function testClone()
+		{
+			$x = clone $this->_registry;
+		}
+
+		public function testNullGet()
+		{
+			$this->assertNull($this->_registry->get('efinsefienfienf'));
+		}
+
+		public function testInterface()
+		{
+			$this->assertTrue(($this->_registry instanceof \Koala\Interfaces\Registry));
+		}
+
+		public function setProvider()
+		{
+			return array(
+				array('integer', 7),
+				array('string', 'Hello World!'),
+				array('array', array(1,2,3)),
+				array('binary', (binary)'Foobar'),
+				array('callable', (function(){ return 'Hello World'; })),
+			);
+		}
+	}
---
+++ tests/run-tests.sh
@@ -0,0 +1,6 @@
+/Applications/MAMP/bin/php/php5.3.6/bin/phpunit \
+	--coverage-html ./coverage/ \
+	--colors \
+	--strict \
+	--repeat 10 \
+	./Koala/RegistryTest.php
